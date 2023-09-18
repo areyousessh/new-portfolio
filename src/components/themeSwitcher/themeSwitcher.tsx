b99a7d62d294352ac4d0dfa367f8e12f1f28dc3e
Sessh <gusalvescontato@gmail.com>
2023-09-18 14:43:29 -0300

    fix: fix themeSwitcher

--- src/components/themeSwitcher/themeSwitcher.tsx
+++ src/components/themeSwitcher/themeSwitcher.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from 'react';
 
 export default function ThemeSwitcher() {
-  const [isDarkMode, setIsDarkMode] = useState(() => {
-    const storedTheme = localStorage.getItem('theme');
-    return storedTheme ? JSON.parse(storedTheme) : window.matchMedia('(prefers-color-scheme: dark)').matches;
-  });
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
+    // Verifica se há uma preferência de tema no localStorage
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme) {
+      setIsDarkMode(storedTheme === 'dark');
+    } else {
+      // Se não houver preferência no localStorage, verifica a preferência do sistema
+      setIsDarkMode(window.matchMedia('(prefers-color-scheme: dark)').matches);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Atualiza a classe no elemento raiz do HTML e armazena a preferência no localStorage
     document.documentElement.classList.toggle('dark', isDarkMode);
-    localStorage.setItem('theme', JSON.stringify(isDarkMode));
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
   }, [isDarkMode]);
 
   function toggleTheme() {
